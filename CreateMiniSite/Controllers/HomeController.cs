655250ba183e95751d5cea8d3801d4a909c95162
Marziue esmaili <marziyeitmarziye@gmail.com>
2024-05-06 20:23:02 +0330

    Recive data with controller page(server) and send this view page and show by table mood

---
+++ CreateMiniSite/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CreateMiniSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string name,string category,int price)
+        {
+            ViewBag.Name     = name;
+            ViewBag.Category = category;
+            ViewBag.Price    = price;
+            return View();
+        }
+    }
+}
